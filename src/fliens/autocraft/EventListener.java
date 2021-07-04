0f9b83c15cb629153ff16885fae95e424dafa229
BuildTools <unconfigured@null.spigotmc.org>
2021-07-04 20:21:29 +0300

    1) Removed deprecated calls to org.bukkit.material.Dispenser, using org.bukkit.block.data.type.Dispenser instead as suggested in Bukkit deprecated list thus fixing the bug with huge lag during Legacy Materials initialisation 2) Added default config file instead of hard-coding config parameters 3) Added configurable crafting cooldown and changed the default to 8 ticks - hopper speed because to me it seems to be the most optimal default. 4) Changed ignoresRedstone to redstoneMode which can disable or control the type of redstone powering (indirect aka through block/direct) 5) Fixed the bug with wrong item number of items being crafted on "Paper" servers (and maybe others, didn`t check) due to Inventory.addItem() rewriting cached craft results. 6) Updated Readme 7) Added license notes to all source code files as suggested in GNU GPLv3 license

--- src/fliens/autocraft/EventListener.java
+++ src/fliens/autocraft/EventListener.java
@@ -1,3 +1,22 @@
+/*  AutoCraft plugin
+ *
+ *  Copyright (C) 2021 Fliens
+ *  Copyright (C) 2021 MrTransistor
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
 package fliens.autocraft;
 
 import java.util.ArrayList;
@@ -15,17 +34,18 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockDispenseEvent;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.plugin.Plugin;
 
 public class EventListener implements Listener{
 	
-	public EventListener(AutoCraft plugin) {
+	public EventListener(Plugin plugin) { // changed to generic Plugin
 		plugin.getServer().getPluginManager().registerEvents(this, plugin);
 	}
 	
 	@EventHandler
 	public void onItemMove(BlockDispenseEvent e) {
 		if(e.getBlock().getType().equals(Material.DISPENSER)) {
-			List<Block> autoCrafters = new ArrayList<Block>();
+			List<Block> autoCrafters = new ArrayList<>();
 
 			for (World world : Bukkit.getWorlds()) {
 				for (Chunk chunk : world.getLoadedChunks()) {
